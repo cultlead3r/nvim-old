96aee3b03a71234bbfad72b268475577983488b7
cultleader <cultleader@cultleader.me>
2022-10-11 23:49:31 -0500

    add python venv field in pyright settings

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -1,6 +1,8 @@
 return {
   settings = {
     python = {
+      -- set python venv
+      -- pythonPath = "/home/cultleader/tmp/venv/bin/python",
       analysis = {
         typeCheckingMode = "off",
       },
